a281eca7aac9bb5f9df9c8bf0138582b2548cb3e
Smart Tool Factory <smarttoolfactory@icloud.com>
2021-07-14 18:28:43 +0300

    Add simplified sample of remember and MutableState of Compose

---
+++ tutorial/src/main/java/com/smarttoolfactory/tutorial/chapter2OOP/Tutorial2_14DelegationComposeRememberMutableState.kt
@@ -0,0 +1,45 @@
+package com.smarttoolfactory.tutorial.chapter2OOP
+
+import kotlin.reflect.KProperty
+
+fun main() {
+
+    val isSelected: MutableState<Boolean> = remember { mutableStateOf(true) }
+    isSelected.value = false
+
+    var selected by remember { mutableStateOf(false) }
+    selected = false
+}
+
+
+/*
+ *  MutableState
+ */
+interface MutableState<T> : State<T> {
+    override var value: T
+}
+
+// Delegation Functions for setting and getting value
+inline operator fun <T> State<T>.getValue(thisObj: Any?, property: KProperty<*>): T = value
+
+inline operator fun <T> MutableState<T>.setValue(thisObj: Any?, property: KProperty<*>, value: T) {
+    this.value = value
+}
+
+interface State<out T> {
+    val value: T
+}
+
+class MutableStateImpl<T>(value: T) : MutableState<T> {
+    override var value: T = value
+}
+
+fun <T> mutableStateOf(value: T): MutableState<T> = MutableStateImpl(value)
+
+/*
+ *  Remember
+ */
+
+inline fun <T> remember(calculation: () -> T): T {
+    return calculation()
+}
